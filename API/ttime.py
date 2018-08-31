56741e84577efec68ceeb04c04600bc69c7be7fe
Denis Belavin <belavin.d.andreevich@gmail.com>
2018-08-31 12:37:43 +0300

    add: ttime.py, test_ttime.py

---
+++ API/ttime.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+
+from time import strftime
+from time import gmtime
+
+__all__ = ['ConvertTime']
+
+
+class ConvertTime(object):
+    """
+    Convert time
+
+    Convert time from int into a string using the specified format.
+
+    t: int(time.time())
+    f: str(), default = '%a, %d %b %Y %H:%M:%S'
+    """
+    @staticmethod
+    def convert(t, f=None):
+        if not isinstance(t, int):
+            raise TypeError()
+        format_ = f or '%a, %d %b %Y %H:%M:%S'
+        if not isinstance(format_, str):
+            raise TypeError()
+        return strftime(format_, gmtime(t))
---
+++ tests/test_ttime.py
@@ -0,0 +1,31 @@
+# -*- coding: utf8 -*-
+
+import pytest
+import time
+from API.ttime import ConvertTime
+
+
+def test_input():
+    t = ConvertTime.convert(int(time.time()))
+    assert t == time.strftime('%a, %d %b %Y %H:%M:%S', time.gmtime(int(time.time())))
+
+
+@pytest.mark.parametrize('f', [
+    '%A, %S',
+    '%H:%M',
+    '%B',
+    '%a, %b, %c'
+])
+def test_format(f):
+    t = ConvertTime.convert(int(time.time()), f)
+    assert t == time.strftime(f, time.gmtime((int(time.time()))))
+
+
+def test_error_type_time():
+    with pytest.raises(TypeError):
+        ConvertTime.convert('')
+
+
+def test_error_type_format():
+    with pytest.raises(TypeError):
+        ConvertTime.convert(int(time.time()), 123)
